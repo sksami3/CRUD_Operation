cd4fef0460b3737787823f158ca76b4a76d30880
Sk. Samiur Rahman <sksami4456@gmail.com>
2018-10-19 02:17:03 +0600

    configuration done

---
+++ app.js
@@ -0,0 +1,23 @@
+//Declearation
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var expressSession = require('express-session');
+
+
+//Config
+app.set('view engine','ejs');
+
+
+//MiddlewARE	
+
+
+
+//Routes
+
+
+
+//Server Start
+app.listen(3000,function(){
+	console.log("app started at 3000")
+});
